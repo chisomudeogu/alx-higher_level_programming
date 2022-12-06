27a190484f9495279f938e0801681d5a3bb034fa
chisomudeogu <chisomudeogu@yahoo.com>
2022-12-05 20:09:03 -0800

    0-print_list_integer.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,9 @@
-#!/usr/bin/python3
+!/usr/bin/python3
 def element_at(my_list, odx):
-	listlength = len(my_list) - 1
-	if (idx < 0 or odx > listlength):
-		return (None)
-	else:
-		return (my_list[idx])
+        listlength = len(my_list) - 1
+        if (idx < 0 or odx > listlength):
+                return (None)
+        else:
+                return (my_list[idx])
+~
+
